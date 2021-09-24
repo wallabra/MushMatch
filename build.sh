84db710bba868e0f1b6a64c9620886506f080124
Gustavo6046 <rehermann6046@gmail.com>
2021-09-24 01:55:15 -0300

    Wrap values in quotes in build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 source ./config.sh
 
 cleanup() {
-    rm -rv MushMatch-$build
+    rm -rv "MushMatch-$build"
 }
 
 ( # Subshell to preserve original working dir
@@ -12,18 +12,18 @@ cleanup() {
     ( # Subshell to exit early on error, to go right into cleanup
         set -e
 
-        mkdir MushMatch-$build
+        mkdir MushMatch-"$build"
 
         for asset in Classes Models Textures Sounds make.ini; do
-            cp -rv MushMatch/$asset MushMatch-$build
+            cp -rv MushMatch/"$asset" MushMatch-"$build"
         done
 
         # Build .u
-        WINEPREFIX=$wineprefix wine $umake MushMatch-$build
+        WINEPREFIX="$wineprefix" wine "$umake" "MushMatch-$build"
 
         # Build .int
         pushd MushMatch
-        ./formatint.sh "$build" "$version" >../System/MushMatch-$build.int
+        ./formatint.sh "$build" "$version" >"../System/MushMatch-$build.int"
         popd
     )
 
