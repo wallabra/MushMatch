db40582539c7b9696858c61d95cfd3cb09dcd6b5
Gustavo6046 <rehermann6046@gmail.com>
2021-09-28 03:21:41 -0300

    Bump version to reflect rebalance and tweaks

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-export version=1.2.0
-export build=20213903
+export version=1.2.1
+export build=20213904
 
 export wineprefix=$HOME/Applications/@wine/ut99
 export umake='Tools/UMake/UMake.exe'
