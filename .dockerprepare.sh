b47e9c27dd875b1fc23a0bcbafb544b744b1ebed
Gustavo6046 <rehermann6046@gmail.com>
2021-10-02 06:00:37 -0300

    Use curl instead of wget

--- .dockerprepare.sh
+++ .dockerprepare.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-wget -c 'http://ut-files.com/index.php?dir=Entire_Server_Download/&file=ut-server-linux-436.tar.gz' -O'/var/tmp/ut-server-linux-436.tar.gz'
-wget -c 'https://github.com/OldUnreal/UnrealTournamentPatches/releases/download/v469b/OldUnreal-UTPatch469b-Linux.tar.bz2' -O'/var/tmp/OldUnreal-UTPatch469b-Linux.tar.bz2'
+curl 'http://ut-files.com/index.php?dir=Entire_Server_Download/&file=ut-server-linux-436.tar.gz' -L -o'/var/tmp/ut-server-linux-436.tar.gz'
+curl 'https://github.com/OldUnreal/UnrealTournamentPatches/releases/download/v469b/OldUnreal-UTPatch469b-Linux.tar.bz2' -L -o'/var/tmp/OldUnreal-UTPatch469b-Linux.tar.bz2'
 tar -xzf /var/tmp/ut-server-linux-436.tar.gz -C /opt
 tar -xjpf /var/tmp/OldUnreal-UTPatch469b-Linux.tar.bz2 -C /opt/ut-server
 rm /var/tmp/ut-server-linux-436.tar.gz /var/tmp/OldUnreal-UTPatch469b-Linux.tar.bz2
