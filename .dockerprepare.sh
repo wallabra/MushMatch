51ac6eff5b8ce1efa8da116aa03476412a4f0b08
Gustavo6046 <rehermann6046@gmail.com>
2021-10-02 05:26:42 -0300

    Fix rm in dockerprepare

--- .dockerprepare.sh
+++ .dockerprepare.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-curl 'http://ut-files.com/index.php?dir=Entire_Server_Download/&file=ut-server-linux-436.tar.gz' -OL -C- --output-dir /var/tmp --create-dirs --retry 5
-curl 'https://github.com/OldUnreal/UnrealTournamentPatches/releases/download/v469b/OldUnreal-UTPatch469b-Linux.tar.bz2'  -OL -C- --output-dir /var/tmp --create-dirs --retry 5
+
+wget -c 'http://ut-files.com/index.php?dir=Entire_Server_Download/&file=ut-server-linux-436.tar.gz' -O'/var/tmp/ut-server-linux-436.tar.gz'
+wget -c 'https://github.com/OldUnreal/UnrealTournamentPatches/releases/download/v469b/OldUnreal-UTPatch469b-Linux.tar.bz2' -O'/var/tmp/OldUnreal-UTPatch469b-Linux.tar.bz2'
 tar -xzf /var/tmp/ut-server-linux-436.tar.gz -C /opt
 tar -xjpf /var/tmp/OldUnreal-UTPatch469b-Linux.tar.bz2 -C /opt/ut-server
-rm 'http://ut-files.com/index.php?dir=Entire_Server_Download/&file=ut-server-linux-436.tar.gz' 'https://github.com/OldUnreal/UnrealTournamentPatches/releases/download/v469b/OldUnreal-UTPatch469b-Linux.tar.bz2'
+rm /var/tmp/ut-server-linux-436.tar.gz /var/tmp/OldUnreal-UTPatch469b-Linux.tar.bz2
